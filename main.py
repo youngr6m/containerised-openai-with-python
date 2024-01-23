3739c168079c8457d726300ed255db7e43cee511
youngr6m <73336799+youngr6m@users.noreply.github.com>
2024-01-23 12:28:54 +0000

    added simple table with max highlight

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import streamlit as st
+import pandas as pd
 
 def main():
     st.write("# Hello, world!")
     st.write("## My first streamlit app")
+
+    dataframe = pd.DataFrame({
+        'first column': [1, 2, 3, 4],
+        'second column': [10, 20, 30, 40]
+    })
+
+    st.dataframe(dataframe.style.highlight_max(axis=0))
+    
 
 if __name__ == "__main__":
     main()
